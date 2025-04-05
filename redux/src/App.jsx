dc999becae4bffb4e443d3b760fc41118f07d611
Ilyes.dragovic <mingo.huncho@gmail.com>
2025-04-05 07:54:07 +0100

    redux

--- redux/src/App.jsx
+++ redux/src/App.jsx
@@ -1,23 +1,11 @@
 import React from "react";
-import CakeContainer from "./components/CakeContainer";
-import HooksCakeContainer from "./components/HooksCakeContainer";
-import IceCreamContainer from "./components/iceCreamContainer";
-import HooksIceCreamContainer from "./components/HooksIceCreamContainer";
-import NewCakeContainer from "./components/NewCakeContainer";
-import ItemContainer from "./components/ItemContainer";
+
 import UserContainer from "./components/UserContainer";
 
 const App = () => {
   return (
     <div>
-      {/* <ItemContainer cake />
-      <ItemContainer IceCream />
-      <HooksCakeContainer />
-      <CakeContainer />
-      ###########
-      <IceCreamContainer />
-      <HooksIceCreamContainer />
-      <NewCakeContainer /> */}
+    
       <UserContainer />
     </div>
   );
